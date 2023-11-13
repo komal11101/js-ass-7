13ad1fee74ab34d979b60d896bb9a5b2670e5ac3
komal11101 <125795810+komal11101@users.noreply.github.com>
2023-11-13 19:41:41 +0530

    ques6.js

--- ques6.js
+++ ques6.js
@@ -1,5 +1,15 @@
-const myObj(obj,N)=
+const obj1 = {
+    id : 12,
+    houseno : 122
+};
+
+function multiply(N)
 {
-id : 21241,
-houseno : 2
+    obj1.id *= N;
+    obj1.houseno *=N;
+
+    console.log(obj1.id);
+    console.log(obj1.houseno);
 }
+
+multiply(2);
